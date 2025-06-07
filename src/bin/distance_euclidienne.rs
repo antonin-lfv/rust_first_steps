d7899dc6cd4512983b32b51073e312533477d94c
Antonin <antoninlefevre45@icloud.com>
2025-06-07 19:33:44 +0200

    init project

---
+++ src/bin/distance_euclidienne.rs
@@ -0,0 +1,48 @@
+use ndarray::Array1;
+
+fn main() {
+    println!("Here we'll calculate the Euclidean distance between two points in 2D space.");
+    let a = vec![1.0, 2.0, 3.0];
+    let b = vec![4.0, 6.0, 3.0];
+
+    let distance = euclidean_distance(&a, &b);
+    println!("Euclidean distance with slices : {}", distance);
+
+    // Utilisation de ndarray
+    let a_nd = Array1::from_vec(a.clone()); // on clone a pour éviter de consommer `a`
+    let b_nd = Array1::from_vec(b.clone()); // on clone b pour éviter de consommer `b`
+    let distance_nd = euclidean_distance_nd(&a_nd, &b_nd);
+    println!("Euclidean distance with ndarray : {}", distance_nd);
+}
+
+/// Calcule la distance euclidienne entre deux vecteurs de même taille en utilisant les slices.
+/// 
+/// # Arguments
+/// * `x` - Le premier vecteur.
+/// * `y` - Le second vecteur.
+/// 
+/// # Returns
+/// * La distance euclidienne entre les deux vecteurs.
+///
+/// # Example
+/// ```
+/// let x = vec![1.0, 2.0];
+/// let y = vec![4.0, 6.0];
+/// let dist = euclidean_distance(&x, &y);
+/// assert!((dist - 5.0).abs() < 1e-6);
+/// ```
+fn euclidean_distance(x: &[f64], y: &[f64]) -> f64 { // &[f64] = slice = un vecteur dont on connaît la taille à l’exécution
+    assert_eq!(x.len(), y.len(), "Les vecteurs doivent avoir la même taille !");
+    let sum_squared_diff: f64 = x.iter() // .iter() = itère sur les éléments du vecteur
+        .zip(y.iter()) // .zip() = associe les éléments des deux vecteurs
+        .map(|(xi, yi)| (xi - yi).powi(2))  // powi(2) = carré d’un f64
+        .sum();
+
+    sum_squared_diff.sqrt()
+}
+
+/// Calcule la distance euclidienne entre deux vecteurs de même taille en utilisant ndarray.
+fn euclidean_distance_nd(x: &Array1<f64>, y: &Array1<f64>) -> f64 {
+    ((x - y).mapv(|v| v.powi(2))).sum().sqrt()  // mapv = applique une fonction à chaque élément du tableau (pas la copie de l'élément)
+    // ou (x - y).mapv(|v| v.powi(2)).scalar_sum().sqrt()
+}
---
+++ src/bin/plot_2D_fonctions.rs
@@ -0,0 +1,40 @@
+use plotly::common::Mode;
+use plotly::{Plot, Scatter};
+use plotly::layout::{Layout, Axis};
+
+fn relu(x: f64) -> f64 {
+    x.max(0.0)
+}
+
+fn main() {
+    let x_vals: Vec<f64> = (-10..=10).map(|x| x as f64 * 0.5).collect();
+    let y_vals_relu: Vec<f64> = x_vals.iter().map(|&x| relu(x)).collect();
+    let y_vals_sigmoid: Vec<f64> = x_vals.iter().map(|&x| 1.0 / (1.0 + (-x).exp())).collect();
+
+    let trace = Scatter::new(x_vals.clone(), y_vals_relu)
+        .mode(Mode::Lines)
+        .name("ReLU");
+
+    let mut plot = Plot::new();
+    plot.add_trace(trace);
+
+    let trace2 = Scatter::new(x_vals.clone(), y_vals_sigmoid).mode(Mode::Lines).name("Sigmoid");
+    plot.add_trace(trace2);
+
+    // Configuration de la mise en page du graphique
+    plot.set_layout(
+    Layout::new()
+        .title("Fonctions d'activation")
+        .x_axis(Axis::new().title("x"))
+        .y_axis(Axis::new().title("f(x)")),
+    );
+
+    use std::process::Command;
+
+    plot.write_html("relu_plot.html");
+
+    Command::new("open")
+        .arg("relu_plot.html")
+        .status()
+        .expect("Échec de l'ouverture du fichier HTML");
+}
